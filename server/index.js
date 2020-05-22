c331d42832824b9daf5eb82987777b26108874f3
faiisll <faisal.ayas72@gmail.com>
2020-05-23 03:26:43 +0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const handle = require('./handlers');
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.json({
+    message: 'hello world'
+}));
+
+app.use(handle.notFound);
+app.use(handle.errors);
+
+
+
+app.listen(port, console.log(`Server started on port ${port}`));
